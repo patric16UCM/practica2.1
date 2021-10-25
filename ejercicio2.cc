3c4f399010149677e2fa0bbf75c69754ccb9ee3d
patric16UCM <93123535+patric16UCM@users.noreply.github.com>
2021-10-25 12:42:23 +0200

    Create ejercicio2.cc

---
+++ ejercicio2.cc
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+ #include <errno.h>
+
+
+using namespace std;
+
+int main(){
+ int resultado=0;
+ resultado = setuid(2);
+ if(resultado == -1){
+     perror("Se ha producido un error");
+     printf(strerror(resultado));
+ }
+
+ return 1;   
+}
